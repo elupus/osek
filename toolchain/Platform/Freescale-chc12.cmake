e98257e37512f0dd1fca13e84c6e9af0807c6e42
Joakim Plate <elupus@ecce.se>
2015-01-29 22:26:37 +0100

    Move some further toolchain info to platform file

--- toolchain/Platform/Freescale-chc12.cmake
+++ toolchain/Platform/Freescale-chc12.cmake
@@ -28,6 +28,33 @@ MESSAGE ("Loaded: Freescale-chc12.cmake")
 #
 
 #
+# We have to setup the object type very early, as this can't be overridden later on.
+#
+SET (CMAKE_C_OUTPUT_EXTENSION     ".o"         CACHE STRING "C compiler object extension.")
+SET (CMAKE_CXX_OUTPUT_EXTENSION   ".o"         CACHE STRING "C++ compiler object extension.")
+
+#
+# Convince CMake we know our compiler works OK.
+# If we don't do this it will do a TRY-COMPILE which will fail to link.
+#
+SET (CMAKE_C_COMPILER_ID_RUN    TRUE)
+SET (CMAKE_C_COMPILER_WORKS     TRUE)
+SET (CMAKE_CXX_COMPILER_ID_RUN  TRUE)
+SET (CMAKE_CXX_COMPILER_WORKS   TRUE)
+
+#
+# Workaround to inhibit CMake from performing a TRY-COMPILE to determine the size of "void *",
+# this size isn't ever used so its actual value doesn't matter.
+#
+SET (CMAKE_SIZEOF_VOID_P 2)
+
+#
+# Don't generate preprocessed or assembler makefile rules for C/C++ source files.
+#
+SET (CMAKE_SKIP_PREPROCESSED_SOURCE_RULES    TRUE)
+SET (CMAKE_SKIP_ASSEMBLY_SOURCE_RULES        TRUE)
+
+#
 # Allow user to easily override the variables from a file rather than the CMake command line.
 #
 INCLUDE ("Freescale-HC12-opt.cmake" OPTIONAL RESULT_VARIABLE _INCLUDED_FREESCALE_HC12_OPT_FILE)
--- toolchain/Toolchain-Freescale-HC12.cmake
+++ toolchain/Toolchain-Freescale-HC12.cmake
@@ -40,30 +40,3 @@ set(CMAKE_OBJDUMP      FALSE)
 include_directories(${HC12_PATH}/lib/hc12c/include)
 link_directories(${HC12_PATH}/lib/hc12c/lib)
 set (HC12_LINK_PRM_FILE ${HC12_PATH}/lib/hc12c/prm/mc9s12e128.prm)
-
-#
-# We have to setup the object type very early, as this can't be overridden later on.
-#
-SET (CMAKE_C_OUTPUT_EXTENSION     ".o"         CACHE STRING "C compiler object extension.")
-SET (CMAKE_CXX_OUTPUT_EXTENSION   ".o"         CACHE STRING "C++ compiler object extension.")
-
-#
-# Convince CMake we know our compiler works OK.
-# If we don't do this it will do a TRY-COMPILE which will fail to link.
-#
-SET (CMAKE_C_COMPILER_ID_RUN    TRUE)
-SET (CMAKE_C_COMPILER_WORKS     TRUE)
-SET (CMAKE_CXX_COMPILER_ID_RUN  TRUE)
-SET (CMAKE_CXX_COMPILER_WORKS   TRUE)
-
-#
-# Workaround to inhibit CMake from performing a TRY-COMPILE to determine the size of "void *",
-# this size isn't ever used so its actual value doesn't matter.
-#
-SET (CMAKE_SIZEOF_VOID_P 2)
-
-#
-# Don't generate preprocessed or assembler makefile rules for C/C++ source files.
-#
-SET (CMAKE_SKIP_PREPROCESSED_SOURCE_RULES    TRUE)
-SET (CMAKE_SKIP_ASSEMBLY_SOURCE_RULES        TRUE)
