c1e4e80212ece5a4ea64ff7ccf2acc66c57dc83e
Joakim Plate <elupus@ecce.se>
2015-02-01 18:37:14 +0100

    Fixed invalid storage/restore of context on HCS12
    Note, PPAGE address may be different on different devices. 0x15 should
    work for S12P set of devices

--- src/Os_Arch_HCS12.c
+++ src/Os_Arch_HCS12.c
@@ -21,11 +21,6 @@
 
 typedef struct Os_Arch_StateType {
     uint16_t sp;
-    uint8_t  ccr;
-#ifdef __GNUC__
-    uint16_t frame;
-    uint16_t d1;
-#endif
  } Os_Arch_StateType;
 
 Os_Arch_StateType  Os_Arch_State[OS_TASK_COUNT];
@@ -42,44 +37,42 @@ void Os_Arch_Init(void)
     Os_Arch_Ctx_Next = &Os_Arch_State_None;
 }
 
+#define PPAGE_ADDRESS "0x15"
+
 #ifdef __HIWARE__
-#define OS_ARCH_STORE()                 \
-    __asm  (                           \
-            "ldx Os_Arch_Ctx_Prev\n"     \
-            "sts           0x0 , X\n"    \
-            "tpa\n"                      \
-            "staa          0x2 , X\n"    \
+#define OS_ARCH_STORE()                                 \
+    __asm  (                                            \
+            "movb " PPAGE_ADDRESS ", 0x1 , -SP\n"       \
+            "ldx Os_Arch_Ctx_Prev\n"                    \
+            "sts           0x0 , X\n"                   \
      )
 
 #define OS_ARCH_RESTORE()                               \
-    __asm (                                            \
+    __asm (                                             \
             "ldx Os_Arch_Ctx_Next\n"                    \
             "lds  0x0 , X\n"                            \
-            "ldaa 0x2 , X\n"                            \
-            "tap\n"                                     \
+            "movb 0x1 , SP+, " PPAGE_ADDRESS "\n"       \
             "movw Os_Arch_Ctx_Next, Os_Arch_Ctx_Prev\n" \
     )
 
 #else
-#define OS_ARCH_STORE()                  \
-    __asm __volatile__ (                 \
-            "movw _.frame, 2   , -SP\n"  \
-            "movw _.d1   , 2   , -SP\n"  \
-            "ldx Os_Arch_Ctx_Prev\n"     \
-            "sts           0x0 , X\n"    \
-            "tpa\n"                      \
-            "staa          0x2 , X\n"    \
+#define OS_ARCH_STORE()                                 \
+    __asm __volatile__ (                                \
+            "movw _.frame, 2   , -SP\n"                 \
+            "movw _.d1   , 2   , -SP\n"                 \
+            "movb " PPAGE_ADDRESS ", 0x1 , -SP\n"       \
+            "ldx Os_Arch_Ctx_Prev\n"                    \
+            "sts           0x0 , X\n"                   \
      )
 
-#define OS_ARCH_RESTORE()                             \
-    __asm __volatile__ (                              \
-            "ldx Os_Arch_Ctx_Next\n"                  \
-            "lds  0x0 , X\n"                          \
-            "ldaa 0x2 , X\n"                          \
-            "tap\n"                                   \
-            "movw 0x2, SP+, _.d1\n"                   \
-            "movw 0x2, SP+, _.frame\n"                \
-            "movw Os_Arch_Ctx_Next, Os_Arch_Ctx_Prev" \
+#define OS_ARCH_RESTORE()                               \
+    __asm __volatile__ (                                \
+            "ldx Os_Arch_Ctx_Next\n"                    \
+            "lds  0x0 , X\n"                            \
+            "movb 0x1, SP+, " PPAGE_ADDRESS "\n"        \
+            "movw 0x2, SP+, _.d1\n"                     \
+            "movw 0x2, SP+, _.frame\n"                  \
+            "movw Os_Arch_Ctx_Next, Os_Arch_Ctx_Prev"   \
     )
 #endif
 
@@ -116,18 +109,30 @@ void Os_Arch_PrepareState(Os_TaskType task)
     Os_Arch_StateType *regs = &Os_Arch_State[task];
     const Os_TaskConfigType *cfg  = &Os_TaskConfigs[task];
     uint8_t   i;
-    uint16_t* ptr = (uint16_t*)((uint16_t)cfg->stack + cfg->stack_size);
 
+    uint8_t* ptr = (uint8_t*)((uint16_t)cfg->stack + cfg->stack_size);
     ptr -= 9;
-    ptr[0] = 0x0u;       /* _d1 */
-    ptr[1] = 0x0u;       /* _frame */
-    ptr[2] = 0x0u;       /* _xy */
-    ptr[3] = 0x0u;       /* _z */
-    ptr[4] = 0x0u;       /* _tmp */
-    ptr[5] = (uint16_t)cfg->entry; /* PC */
-    ptr[6] = 0x0u;       /* D */
-    ptr[7] = 0x0u;       /* X */
-    ptr[8] = 0x0u;       /* Y */
+    ptr[0] = 0x0u;       /* CCR */
+    ptr[1] = 0x0u;       /* B */
+    ptr[2] = 0x0u;       /* A */
+    ptr[3] = 0x0u;       /* X high */
+    ptr[4] = 0x0u;       /* X low  */
+    ptr[5] = 0x0u;       /* Y high */
+    ptr[6] = 0x0u;       /* Y low  */
+    ptr[7] = ((uint32_t)cfg->entry >> 16u) & 0xffu;
+    ptr[8] = ((uint32_t)cfg->entry >>  8u) & 0xffu;
+
+    ptr -= 1;
+    ptr[0] = ((uint32_t)cfg->entry >>  0u) & 0xffu; /* PPAGE */
+
+#ifdef __GNUC__
+    ptr -= 5*sizeof(int16_t);
+    ((int16_t*)ptr)[0] = 0x0u; /* _d1 */
+    ((int16_t*)ptr)[1] = 0x0u; /* _frame */
+    ((int16_t*)ptr)[2] = 0x0u; /* _xy */
+    ((int16_t*)ptr)[3] = 0x0u; /* _z */
+    ((int16_t*)ptr)[4] = 0x0u; /* _tmp */
+#endif
+
     regs->sp  = (uint16_t)ptr;
-    regs->ccr = 0x0u;
 }
