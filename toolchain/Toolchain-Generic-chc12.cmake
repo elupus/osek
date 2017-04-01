cf691ec2dd328450da27298f72565188d187bf78
Joakim Plate <joakim.plate@gmail.com>
2017-04-01 20:44:57 +0200

    Simplify and rework hc12 toolchain files

--- toolchain/Toolchain-Generic-chc12.cmake
+++ toolchain/Toolchain-Generic-chc12.cmake
@@ -6,7 +6,6 @@
 # Important, make sure you include the ":string" type option, otherwise the entries won't be placed in the
 # cache causing later problems when make automatically invokes a CMake rebuild.
 #
-INCLUDE(CMakeForceCompiler)
 MESSAGE ("Loaded: Toolchain-Generic-chc12 .cmake")
 
 # Look for modules in this path
@@ -17,9 +16,11 @@ IF (NOT DEFINED HC12_PATH)
     find_path(HC12_PATH NAMES Prog/chc12.exe
                         PATHS "/Users/joakim/.wine/drive_c/P"
                               "/c/Program Files/Freescale/CWS12v5.1/"
+                              "/c/Freescale/CWS12v5.1/"
                               "/cygdrive/c/Program Files/Freescale/CWS12v5.1/"
                               "P:"
                               "C:/Program Files/Freescale/CWS12v5.1/"
+                              "C:/Freescale/CWS12v5.1/"
                         NO_DEFAULT_PATH)
 ENDIF()
 
@@ -29,14 +30,19 @@ SET (CMAKE_SYSTEM_NAME          "Generic")
 SET (CMAKE_SYSTEM_VERSION       "0.0")
 SET (CMAKE_SYSTEM_PROCESSOR     "hc12")
 
-SET (HC12_CORE         HCS12)
-SET (HC12_MEMORY_MODEL Banked)
+SET (HC12_CORE         "HCS12" CACHE STRING "")
+SET (HC12_MEMORY_MODEL "b"     CACHE STRING "")
 
 #
 # Toolchain - Compilers, librarian (archiver) and linker.
 #              Piper is a Freescale command line tool that is used to redirect their GUI tools output to stdout.
 #
-CMAKE_FORCE_C_COMPILER  ("${HC12_PATH}/Prog/chc12.exe"     chc12)
-CMAKE_FORCE_CXX_COMPILER("${HC12_PATH}/Prog/chc12.exe"     chc12)
-set(CMAKE_AR             "${HC12_PATH}/Prog/libmaker.exe")
-set(CMAKE_LINKER         "${HC12_PATH}/Prog/linker.exe")
+set(CMAKE_C_COMPILER        "chc12.exe")
+set(CMAKE_C_COMPILER_ID     "chc12")
+set(CMAKE_C_COMPILER_ID_RUN 1)
+set(CMAKE_C_PLATFORM_ID     "")
+
+set(CMAKE_CXX_COMPILER       "chc12.exe")
+set(CMAKE_CXX_COMPILER_ID    "chc12")
+set(CMAKE_CXX_COMPILER_ID_RUN 1)
+set(CMAKE_CXX_PLATFORM_ID     "")
